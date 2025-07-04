ba98639e89f1a9fad33154bdcfa297d0015f3acb
mahmud <mahmudmodibbo21@gmail.com>
2025-07-04 15:42:00 +0100

    SEO test 3

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'production' => false,
+    'production' => true,
     'baseUrl' => '',
     'title' => 'African Institute for Solutions and Development',
     'description' => 'Empowering African communities through innovative, evidence-based health solutions and partnerships. AISD drives health equity and sustainable development across Africa.',
